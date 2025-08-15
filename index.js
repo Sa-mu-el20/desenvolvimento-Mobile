86ec00498879b5f324e8e259f1022df3e243ee6b
Samuel Gomes Alves <20242INN022@cefetes.br>
2025-08-14 21:34:18 -0300

    aula1 - conceito de funções

---
+++ index.js
@@ -0,0 +1,113 @@
+//função normal - usar a palavra function seguido do nome da função
+    function imc(peso, altura){
+        console.log("seu imc(indice de massa corporal é " + peso / altura )
+       
+        
+}
+
+let peso = 65
+let altura = 1.75
+        
+console.log(peso/altura)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//funcao anonima - não tem o nome da função mas tem a palavra reservada "function"
+//const exibirNota = function (nota){
+//    console.log("A minha nota é: "+ nota);
+
+
+//}
+
+
+
+
+
+
+
+// arrow functions - muda a forma de escrever a função, deixa de usar "function"
+
+const dividir = (peso, altura) => {
+
+    
+   
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function direta - expressão
+
+const multiplicar = (num1,num2) => num1 * num2 ;
+
+//Elabore uma função que recebe o peso e altura e exiba no console
+// O IMC de qaulquer pessoa, faça usando a função em modo normal,
+//anonima, arrow function,
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const idade = 20
+//const nota = 10
+//exibirNota(idade)
+//saudacao("Samuel ", idade)
+//somar(num1,num2)
+//console.log(multiplicar(num1,num2))
